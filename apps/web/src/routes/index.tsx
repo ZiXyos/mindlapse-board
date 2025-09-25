17343dfc53f3f70aa63946157435d480ec8127a0
ZiXyos <killian.moudery@gmail.com>
2025-09-25 21:53:23 +0200

    feat(ui): add navbar

--- apps/web/src/routes/index.tsx
+++ apps/web/src/routes/index.tsx
@@ -4,6 +4,7 @@ import { create } from "zustand";
 
 import reactLogo from "../assets/react.svg";
 import viteLogo from "/vite.svg";
+import Layout from "../layout.tsx";
 
 interface CounterState {
   count: number
@@ -24,31 +25,33 @@ function Index() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={increase}>
-          count is {count}
-        </button>
-        <div className="card">
-          <button onClick={reset}>
-            reset counter
-          </button>
-        </div>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <Layout>
+            <div>
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={increase}>
+                    count is {count}
+                </button>
+                <div className="card">
+                    <button onClick={reset}>
+                        reset counter
+                    </button>
+                </div>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </Layout>
     </>
   );
 }
